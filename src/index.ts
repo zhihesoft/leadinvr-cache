254418b678ff9a56fab3d439b4de9a4be8238874
stanley <houyuqing@hotmail.com>
2025-04-16 15:57:08 +0800

    project setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { CacheModule } from "./lib/cache.module";
 export { CacheService } from "./lib/cache.service";
+export { Cached, RevokeCache } from "./lib/cached.decorator";
+export { CachedMeta } from "./lib/cached.meta";
 
