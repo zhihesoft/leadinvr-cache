ba06a0032fbb06374bc4916f5fdbe20cb5e7d4c5
stanley <houyuqing@hotmail.com>
2025-06-16 19:51:01 +0800

    rename export cached and revokecached

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { CacheModule } from "./lib/cache.module";
 export { CacheService } from "./lib/service/cache.service";
-export { HttpCache as Cached, RevokeHttpCache as RevokeCache } from "./lib/service/cached.decorator";
+export { HttpCache, RevokeHttpCache } from "./lib/service/cached.decorator";
 export { CachedMeta } from "./lib/service/cached.meta";
-
